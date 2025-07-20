31c94a8794cf4b6fae0cb2ce80338291cb54b615
sekretsekret <chingiz.kasimov0537@mail.ru>
2025-07-20 13:37:32 -0700

    task_3

--- src/main/kotlin/Main.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    var numberOfOrder = 75
-    val greeting = "Добро пожаловать в интернет-магазин!"
-    println(greeting)
-    println(numberOfOrder)
-
-}
---
+++ src/main/kotlin/lesson_2.kt
@@ -0,0 +1,8 @@
+fun main () {
+    val a: Float = 3f
+    val b: Float = 4f
+    val c: Float = 3f
+    val d: Float = 5f
+    val sum = (a+b+c+d)/4
+    println ("Средний балл по английскому: $sum ")
+}
