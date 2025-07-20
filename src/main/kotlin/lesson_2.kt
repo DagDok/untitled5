a6a28b28b823f2e898693e88ab10409133ce6c6e
sekretsekret <chingiz.kasimov0537@mail.ru>
2025-07-20 15:29:32 -0700

    task_5

--- src/main/kotlin/lesson_2.kt
+++ src/main/kotlin/lesson_2.kt
@@ -1,15 +1,11 @@
-fun main () {
-    val crystal = 7
-    val iron = 11
-    val buff = 0.2
-    val crystalBuff = (crystal*buff).toInt()
-    val ironBuff = (iron*buff).toInt()
-    println("С учетом бонуса Кристаллы: $crystalBuff")
-    println("С учетом бонуса Железо: $ironBuff")
+import com.sun.jdi.Value
+import kotlin.math.pow
 
-
-
-
-
+fun main() {
+    val money = 70000
+    val percent = 16.7 / 100
+    val year = 20
+    var result = money * (1 + percent / 1).pow(year)
+    println(String.format("%.3f", result))
 
 }
