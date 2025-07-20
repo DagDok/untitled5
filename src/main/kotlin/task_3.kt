a34be3ae4c8de1efb523eb35c37a423b12318145
sekretsekret <chingiz.kasimov0537@mail.ru>
2025-07-19 19:59:25 -0700

    task_3

---
+++ src/main/kotlin/task_3.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+
+    println("Год полета: $year")
+    println("$hour:$minute")
+
+    hour = 10
+    minute = 55
+
+    println("$hour:$minute")
+
+}
